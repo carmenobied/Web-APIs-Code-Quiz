5e057169a473d0d3305531b4cbdca674b3ef7e4d
Carmen Obied <carmen.obied@gmail.com>
2020-04-23 17:04:03 -0700

    stored questions as an array of objects in a separe file names questions.js

---
+++ questions.js
@@ -0,0 +1,26 @@
+// Questions array for code quiz
+var quizQuestions = [
+    {
+      question : "Question 1: HTML related",
+      imageSrc : "",
+      answerChoiceA : "",
+      answerChoiceB : "",
+      answerChoiceC : "",
+      correctChoice : "B"
+    },{
+      question : "Question 2: CSS related",
+      imageSrc : "",
+      answerChoiceA : "",
+      answerChoiceB : "",
+      answerChoiceC : "",
+      correctChoice : "C"
+    },{
+      question : "Question 3: JavaScript related",
+      imageSrc : "",
+      answerChoiceA : "",
+      answerChoiceB : "",
+      answerChoiceC : "",
+      correctChoice : "A"
+     }
+];
+
